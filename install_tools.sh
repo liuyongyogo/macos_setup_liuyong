c961af4eaac45b11426ecc90292948619d1fb57b
liuyongyogo <43092356+liuyongyogo@users.noreply.github.com>
2020-03-10 22:49:43 +0800

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,14 @@
+
+brew install coreutils fish git go mosh the_silver_searcher vim wget unrar tree cmake boost bat autoconf aria2 diff-so-fancy ctags
+brew cask install google-chrome
+
+brew cask install alfred bartender gpg-suite iina istat-menus iterm2 keka mattermost microsoft-office notion qlcolorcode qlmarkdown qlstephen qlvideo quicklook-csv quicklook-json visual-studio-code webpquicklook zoomus
+
+brew cask install visual-studio-code
+
+ brew install coreutils bash fish git tmux tree vim wget
+ 
+ 
+ brew tap PX4/homebrew-px4
+ brew install stlink pyenv gcc-arm-none-eabi autoconf automake pkg-config coreutils
+
